507f26eca5d45d048ae294d5188b5ae0de1107c5
Nur Askiah <askiahnur1@gmail.com>
2020-10-10 14:58:49 +0800

    add Register-API

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt');
 const Joi = require('joi');
 
 const jwtKey = process.env.JWT_KEY;
+const saltRounds = 10;
 
 // --------------------------------LOGIN----------------------------
 exports.login = async (req, res) => {
@@ -40,6 +41,57 @@ exports.login = async (req, res) => {
 
     res.send({
       message: 'Login success!',
+      data: {
+        email,
+        token,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: 'Internal Server Error',
+    });
+  }
+};
+
+// --------------------------------REGISTER----------------------------
+exports.register = async (req, res) => {
+  try {
+    const { email, password, fullName, gender, phone, address } = req.body;
+
+    const schema = Joi.object({
+      email: Joi.string().email().required(),
+      password: Joi.string().min(8).required(),
+      fullName: Joi.string().min(3).required(),
+      gender: Joi.string().min(4).required(),
+      phone: Joi.string().min(6).required(),
+      address: Joi.string().min(6).required(),
+    });
+
+    const { error } = schema.validate(req.body);
+    if (error)
+      return res.status(400).send({ message: error.details[0].message });
+
+    const isExist = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (isExist)
+      return res.status(400).send({ message: 'Email has registered' });
+
+    const hash = await bcrypt.hash(password, saltRounds);
+
+    const newUser = await User.create({
+      ...req.body,
+      password: hash,
+    });
+
+    const token = jwt.sign({ id: newUser.id }, jwtKey);
+
+    res.send({
+      message: 'Registration success!',
       data: {
         email,
         token,
--- src/routers/routes.js
+++ src/routers/routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const { login } = require('../controllers/auth');
+const { login, register } = require('../controllers/auth');
 router.post('/login', login);
+router.post('/register', register);
 
 module.exports = router;
