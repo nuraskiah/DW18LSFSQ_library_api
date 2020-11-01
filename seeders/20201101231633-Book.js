368765f5c5dcb765ae26fe08fcd5913605f5b4f5
Nur Askiah <askiahnur1@gmail.com>
2020-11-02 07:24:48 +0800

    update

--- seeders/20201101231633-Book.js
+++ seeders/20201101231633-Book.js
@@ -18,6 +18,8 @@ module.exports = {
             'https://res.cloudinary.com/nuraskiah/image/upload/v1604229043/library/covers/cover-1604229042320.png',
           file: 'file-1604271675739.epub',
           status: 'Approved',
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
       ],
       {}
