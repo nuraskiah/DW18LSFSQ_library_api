a2159b72fd66fdc2bb6fc8766912b3ace4bb18b3
Nur Askiah <askiahnur1@gmail.com>
2020-11-02 07:22:56 +0800

    update

---
+++ seeders/20201101231633-Book.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      'Books',
+      [
+        {
+          title: 'The A.B.C. Murders',
+          author: 'Agatha Christie',
+          publication: 'January 1952',
+          categoryId: 4,
+          userId: 1,
+          pages: 324,
+          isbn: '978006153673',
+          about: 'murders',
+          cover:
+            'https://res.cloudinary.com/nuraskiah/image/upload/v1604229043/library/covers/cover-1604229042320.png',
+          file: 'file-1604271675739.epub',
+          status: 'Approved',
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Books', null, {});
+  },
+};
